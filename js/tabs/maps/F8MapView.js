f488ff23a044578f4836b8c405e38fb3575fc993
João Paulo Ros <joaopaulo@premiersoft.net>
2016-04-13 16:13:39 -0300

    Corrected crash from F8MapView: When the user accesses for the first time without connection, and opens the Map tab, the app crashes and never recover (even with internet).

--- js/tabs/maps/F8MapView.js
+++ js/tabs/maps/F8MapView.js
@@ -48,6 +48,20 @@ class F8MapView extends React.Component {
 
   render() {
     const {map1, map2} = this.props;
+
+	var map1Name = 'Overview';
+	var map2Name = 'Developer Garage';
+
+  	var readerEmptyListMap1 = <View />;
+  	var readerEmptyListMap2 = <View />;
+  	if(map1 && map2) {
+  		readerEmptyListMap1 = <MapView map={map1} />;
+  		readerEmptyListMap2 = <MapView map={map2} />;
+  		
+  		map1Name = map1.name;
+  		map2Name = map2.name;
+  	}
+	    
     return (
       <View style={styles.container}>
         <ListContainer
@@ -55,12 +69,12 @@ class F8MapView extends React.Component {
           backgroundImage={require('./img/maps-background.png')}
           backgroundColor={'#9176D2'}>
           <PureListView
-            title={map1.name}
-            renderEmptyList={() => <MapView map={map1} />}
+            title={map1Name}
+            renderEmptyList={() => readerEmptyListMap1}
           />
           <PureListView
-            title={map2.name}
-            renderEmptyList={() => <MapView map={map2} />}
+            title={map2Name}
+            renderEmptyList={() => readerEmptyListMap2}
           />
         </ListContainer>
         <F8Button
