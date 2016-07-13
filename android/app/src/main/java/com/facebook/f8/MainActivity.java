90d0bb75eae2d3b4dfec55a552b9b7e4560581c3
teamDST <dustin@dstnation.com>
2016-07-13 17:08:43 +0900

    CallbackManager to Application static member
    CallbackManager to Application static member

--- android/app/src/main/java/com/facebook/f8/MainActivity.java
+++ android/app/src/main/java/com/facebook/f8/MainActivity.java
@@ -28,8 +28,6 @@ import com.facebook.react.ReactActivity;
 import android.content.Intent;
 
 public class MainActivity extends ReactActivity {
-    private CallbackManager mCallbackManager;
-
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
@@ -42,7 +40,7 @@ public class MainActivity extends ReactActivity {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+        MainApplication.getCallbackManager().onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
